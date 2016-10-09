dc0b4289a97d15ed9a3a4a70bb2293c0dc0892d9
jmgooden94 <jmgooden94@gmail.com>
2016-10-09 17:40:05 -0500

    got the login page to show, but still can't get login-page.js to load

--- App/platforms/browser/www/loginPage/login-page.js
+++ App/platforms/browser/www/loginPage/login-page.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var LoginPage = function(){
     var loginPage = {
         self: null,
@@ -27,3 +28,26 @@ var LoginPage = function(){
 }
 
 LoginPage();
+=======
+var loginPage = {
+    // LoginPage constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Register submit button event handler
+    bindEvents: function() {
+        $('.button').on('click', this.onSubmit)
+    },
+    // On Submit button push
+    onSubmit: function(e) {
+        e.preventDefault();
+        // Call to authentication API goes here
+    },
+
+    renderView: function() {
+        document.getElementByID("view").innerHTML='<object type="text/html" data="login-page-template.html"></object>';
+    }
+};
+
+loginPage.initialize();
+>>>>>>> got the login page to show,
--- App/www/loginPage/login-page.js
+++ App/www/loginPage/login-page.js
@@ -1,29 +1,21 @@
-var LoginPage = function(){
-    var loginPage = {
-        self: null,
-        initialize: function() {
-            self = this;
-            this.bindEvents();
-            self.renderView();
-        },
-        // Register submit button event handler
-        bindEvents: function() {
-            $('.button').on('click', this.onSubmit)
-        },
-        // On Submit button push
-        onSubmit: function(e) {
-            e.preventDefault();
-            // Call to authentication API goes here
-        },
+var loginPage = {
+    // LoginPage constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Register submit button event handler
+    bindEvents: function() {
+        $('.button').on('click', this.onSubmit)
+    },
+    // On Submit button push
+    onSubmit: function(e) {
+        e.preventDefault();
+        // Call to authentication API goes here
+    },
 
-        renderView: function() {
-            $("#view").load("./loginPage/login-page-template.html", function(data) {
-                $('#view').html(data);
-            });
-        }
+    renderView: function() {
+        document.getElementByID("view").innerHTML='<object type="text/html" data="login-page-template.html"></object>';
     }
-    loginPage.initialize();
-    return loginPage;
-}
+};
 
-LoginPage();
+loginPage.initialize();
