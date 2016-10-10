09670ae110b950d767121aee9cdc1aa30f00e01b
Jonathan Gooden <jmgooden94@gmail.com>
2016-10-10 10:51:12 -0500

    updated from master

--- App/platforms/browser/www/loginPage/login-page.js
+++ App/platforms/browser/www/loginPage/login-page.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-var LoginPage = function(){
-    var loginPage = {
-        self: null,
-        initialize: function() {
-            self = this;
-            this.bindEvents();
-            self.renderView();
-        },
-        // Register submit button event handler
-        bindEvents: function() {
-            $('.button').on('click', this.onSubmit)
-        },
-        // On Submit button push
-        onSubmit: function(e) {
-            e.preventDefault();
-            // Call to authentication API goes here
-        },
-
-        renderView: function() {
-            $("#view").load("./loginPage/login-page-template.html", function(data) {
-                $('#view').html(data);
-            });
-        }
-    }
-    loginPage.initialize();
-    return loginPage;
-}
-
-LoginPage();
-=======
 var loginPage = {
     // LoginPage constructor
     initialize: function() {
@@ -45,14 +14,6 @@ var loginPage = {
     },
 
     renderView: function() {
-        console.log("IN RENDERVIEW");
         document.getElementById("view").innerHTML='<object type="text/html" data="../loginPage/login-page-template.html"></object>';
     }
-<<<<<<< HEAD
 };
-
-loginPage.initialize();
->>>>>>> got the login page to show,
-=======
-};
->>>>>>> got login page to load from loginpage.js instead of index.js; added jquery
